a1bca3c9d5a3de1e18bc6910195a58c3a62f6982
dungdanghung <dungprof8.0@gmail.com>
2024-01-18 13:02:23 +0700

    update create product

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json.Serialization;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
 
+using System.Threading.Tasks;
 
 namespace Store.Controllers;
 
@@ -15,10 +17,12 @@ public class Admin : BaseController
 {
     private int perPage = 21;
     [HttpGet]
-    public IActionResult Index()
+    public ActionResult Index()
     {
         return View();
     }
+
+
 
     public async Task<ActionResult> Listproduct(string page)
     {
@@ -36,6 +40,9 @@ public class Admin : BaseController
 
         if (pageNumber > 0) products = await queryProducts.Skip((pageNumber - 1) * this.perPage).Take(this.perPage).ToListAsync();
         else products = await queryProducts.Take(this.perPage).ToListAsync();
+
+        var categorys = await dbContext.Categories.ToListAsync();
+        ViewData["category"] = categorys;
         ViewData["products"] = products;
         ViewData["total"] = total;
         ViewData["from"] = from;
@@ -44,6 +51,24 @@ public class Admin : BaseController
         ViewData["nextUrl"] = to < total ? $"/Admin/Listproduct?page={pageNumber + 1}" : null;
         return View();
     }
+
+    public class MyFormModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // Add other properties as needed
+    }
+
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(Product product)
+    {
+
+
+        return Json("sdfsf");
+    }
+
 
     public async Task<ActionResult> ProductDetail(int id)
     {
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Store.Models
 {
@@ -13,15 +15,21 @@ namespace Store.Models
         }
 
         public int Id { get; set; }
+        [Required]
         public string ProductName { get; set; } = null!;
         public int? SupplierId { get; set; }
+        [Required]
         public int? CategoryId { get; set; }
         public int ViewCount { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public int Quantity { get; set; }
         public int SoldQuantity { get; set; }
         public decimal? Discount { get; set; }
+        [Required]
         public string? Images { get; set; }
+        [Required]
         public string? Description { get; set; }
 
         public virtual Category? Category { get; set; }
